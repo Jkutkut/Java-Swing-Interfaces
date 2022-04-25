1f2e818226f6e9af3ba520b0062f6360f6248e37
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-25 09:33:05 +0200

    menuBar added

--- src/dam/y19_20/b/view/View.java
+++ src/dam/y19_20/b/view/View.java
@@ -24,6 +24,8 @@ public class View extends JFrame {
     private JTextArea txtaFormDesc;
     private JButton btnFormAdd;
     private JLabel lblFormError;
+    private JMenuItem menuiAdd;
+    private JMenuItem menuiConsultar;
 
     public View() {
         setTitle(TITLE);
