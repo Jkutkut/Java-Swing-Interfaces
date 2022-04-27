eefa8fc1ffe4680ba20695ee61edb5f3422b188b
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-27 14:59:18 +0200

    basic main added

---
+++ src/dam/y21_22/e/main/Main.java
@@ -0,0 +1,27 @@
+package dam.y21_22.e.main;
+
+import dam.y21_22.e.model.ListaCorredores;
+import dam.y21_22.e.view.mainMenu.View;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ListaCorredores listaRecetas = new ListaCorredores();
+
+                View ventana = new View(listaRecetas);
+//                AddCorredor addMenu = new AddCorredor(listaRecetas);
+//                SeeReceta seeMenu = new SeeReceta(listaRecetas);
+
+//                Controller controlador = new Controller(ventana, addMenu, seeMenu);
+//                ventana.setControlador(controlador);
+//                addMenu.setControlador(controlador);
+//                seeMenu.setControlador(controlador);
+                ventana.setVisible(true);
+            }
+        });
+    }
+}
